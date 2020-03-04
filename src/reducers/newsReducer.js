83fac7039d797016f21551f3294a04a17e3d1742
David White <dwhite0627@gmail.com>
2020-03-03 23:12:34 -0500

    Add placeholder reducer actions

--- src/reducers/newsReducer.js
+++ src/reducers/newsReducer.js
@@ -1,11 +1,41 @@
+// Action type imports
+import {
+  FETCH_STORY_START,
+  FETCH_STORY_SUCCESS,
+  FETCH_STORY_FAIL,
+  FETCH_ALL_STORIES_START,
+  FETCH_ALL_STORIES_SUCCESS,
+  FETCH_ALL_STORIES_FAIL,
+  GET_NTH_STORIES
+} from "./../constants/ActionTypes";
+
 const initialState = {
-  articleIds: [],
-  articles: []
+  articleIds: {
+    ids: [],
+    size: 10,
+    cursor: 0
+  },
+  articles: [],
+  error: '',
+  isFetching: false
 };
 
 export const newsReducer = (state = initialState, action) => {
-  switch(action.type) {
-
+  switch (action.type) {
+    case FETCH_STORY_START:
+      return state;
+    case FETCH_STORY_SUCCESS:
+      return state;
+    case FETCH_STORY_FAIL:
+      return state;
+    case FETCH_ALL_STORIES_START:
+      return state;
+    case FETCH_ALL_STORIES_SUCCESS:
+      return state;
+    case FETCH_ALL_STORIES_FAIL:
+      return state;
+    case GET_NTH_STORIES:
+      return state;
     default:
       return state;
   }
