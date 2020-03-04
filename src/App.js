dc7b481a18596c90dcc5bc6f38070a984ceaff61
David White <dwhite0627@gmail.com>
2020-03-03 23:11:13 -0500

    Add the Cards component to App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 
+// Component imports
+import Cards from './components/Cards';
+
 function App() {
   return (
     <div className="App">
-     
+     <Cards />
     </div>
   );
 }
